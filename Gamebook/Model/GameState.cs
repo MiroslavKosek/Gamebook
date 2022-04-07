760a12081714679f87c640bc9e66012d1abc2138
MiroslavPluhar <miroslav.pluhar.020@pslib.cz>
2022-04-07 19:05:14 +0200

    pt.8

--- Gamebook/Model/GameState.cs
+++ Gamebook/Model/GameState.cs
@@ -5,7 +5,7 @@ namespace Gamebook.Model
     public class GameState
     {
 
-        public int HP { get; set; } = 10; // počet životů postavy
+        public int HP { get; set; } = 100; // počet životů postavy
         public int Armor { get; set; } = 0; // počet armoru postavy
         public int Damage { get; set; } = 1; // počet útočného poškození
         public bool HasArmor { get; set; } = false; // má vyrobený armor
