1f568df1ac1808b6fbd853eff523ed44a2b2899d
MiroslavPluhar <miroslav.pluhar.020@pslib.cz>
2022-06-06 18:51:10 +0200

    pt.44 od Viky

--- Gamebook/Model/LocationProvider.cs
+++ Gamebook/Model/LocationProvider.cs
@@ -8,19 +8,19 @@ namespace Gamebook.Model
         public List<Location> Locations { get; set; } = new List<Location>
         {
             new Location("Home", "You are looking on your shelf, where the dragon egg must be."),//0  ///*
-            new Location("Road", "You are on road."),//OW 1  ///*
-            new Location("Nether", "You are in nether."),//2 ///*
+            new Location("Road", "You went to a crossroads. it is a beautiful day."),//OW 1  ///*
+            new Location("Nether", "Welcome to hell buddy."),//2 ///*
             new Location("Mine", "You are not scared to enter. You have been here before."),//3 ///*
-            new Location("Encounter", ""),//OW//4 ///*
+            new Location("Encounter", "Kill It, before it kills you"),//OW//4 ///*
             new Location("Village", "After long trip, you finally found a Village."),//5 ///*
             new Location("Stronghold", "You are finally here, you can finish your journey."),//6 ///*
             new Location("End portal", "What a lucky day today. You don't have to go to the Fortress in Nether."),//7 ////*
-            new Location("Encounter", ""),//Nether//8 ///*
-            new Location("Fortress", "Let's get some blaze rods."),//9///*
+            new Location("Encounter", "Kill It, before it kills you"),//Nether//8 ///*
+            new Location("Fortress", "I'm surprised how easy you are to get here."),//9///*
             new Location("Road", "You are on road in Nether."),//Nether//10
-            new Location("Nether Portal", "You are on your way to the nether."),//11 ///*
-            new Location("Village house", "You have entered the house in Village."),//12///*
-            new Location("Nether Portal to the Stronghold", "Portal to the Stronghold."),//13 
+            new Location("Nether Portal", "You've seen it many times, but it's still fascinating."),//11 ///*
+            new Location("Village house", "You found a villager. You can try to trade with him."),//12///*
+            new Location("Nether Portal to the Stronghold", "Portal to the Stronghold, let's go."),//13 
             new Location("End"," Welcome in End."),//14 ///*
             new Location("Blacksmith", "Try to ask him, if he has some bow for you.") //15
         };
@@ -28,35 +28,35 @@ namespace Gamebook.Model
         public List<Connection> Connections { get; set; } = new List<Connection>
         {
             new Connection(0, 0),
-            new Connection(0, 1, "Walk out from home"),//z domu na cetsu
-            new Connection(11, 2, "Go through Nether portal"),//z cesty do portalu
-            new Connection(1, 3, "Go to Mine"),// z cesty do dolu
-            new Connection(1, 4, "Continue on a Road"),//pokracovat v ceste a enemy
-            new Connection(1, 11, "Go to Nether portal"),//z cesty do portalu
-            new Connection(4, 5, "Go to Village"),//z cesty/enemy do vesnice
-            new Connection(5, 12, "Enter the house"),//dum ve versnici,
-            new Connection(5, 15, "Go to Blacksmith"),
-            new Connection(15, 5, "Go back"),
-            new Connection(12, 5,"Go outside"),
-            new Connection(4, 6, "Go to Stronghold"), // z cesty do strongholdu
-            new Connection(6, 7, "Go to End portal"),//stronhgold skrz portal
+            new Connection(0, 1, "Road"),//z domu na cetsu
+            new Connection(11, 2, "Nether"),//z cesty do portalu
+            new Connection(1, 3, "Mine"),// z cesty do dolu
+            new Connection(1, 4, "Road"),//pokracovat v ceste a enemy
+            new Connection(1, 11, "Nether portal"),//z cesty do portalu
+            new Connection(4, 5, "Village"),//z cesty/enemy do vesnice
+            new Connection(5, 12, "House"),//dum ve versnici,
+            new Connection(5, 15, "Blacksmith"),
+            new Connection(15, 5, "Village"),
+            new Connection(12, 5,"Village"),
+            new Connection(4, 6, "Stronghold"), // z cesty do strongholdu
+            new Connection(6, 7, "End portal"),//stronhgold skrz portal
             new Connection(2, 8, "Continue on Road"),//portal -enemy -  cesta nether
-            new Connection(8, 10, "Go to Road"),//cesta v nether
-            new Connection(10, 13, "Go to Nether portal to Stronghold"),//cesta k portalu v netheru smer stronghold
-            new Connection(10, 9, "Go to the Fortress"),//z cesty do fortress
-            new Connection(13, 6, "Go through Nether portal"),//nether do strongholdu
-            new Connection(7, 14, "Go through End portal"),// weeee
-            new Connection(6, 4, "Go back on Road"),//z pet na cestu ze strongholdu
-            new Connection(10, 2, "Go back to Portal"),//cesta zpet k portalu
-            new Connection(9, 10,"Return to Road in Nether"), //ze strongholdu na cestu
-            new Connection(6, 13, "Go back to Nether"), //ze strongholdu do netheru
-            new Connection(13, 10, "Return to Road"),//portal cesta nether
-            new Connection(5, 1, "Go back on Road to the Home"),//vratit se zpet na ceste
-            new Connection(1, 0, "Go back Home"),//z cesty domu
+            new Connection(8, 10, "Road"),//cesta v nether
+            new Connection(10, 13, "Portal to the Stronghold"),//cesta k portalu v netheru smer stronghold
+            new Connection(10, 9, "Fortress"),//z cesty do fortress
+            new Connection(13, 6, "Stronghold"),//nether do strongholdu
+            new Connection(7, 14, "End portal"),// weeee
+            new Connection(6, 4, "Road"),//z pet na cestu ze strongholdu
+            new Connection(10, 2, "Nether"),//cesta zpet k portalu
+            new Connection(9, 10,"Road"), //ze strongholdu na cestu
+            new Connection(6, 13, "Nether"), //ze strongholdu do netheru
+            new Connection(13, 10, "Road"),//portal cesta nether
+            new Connection(5, 1, "Road"),//vratit se zpet na ceste
+            new Connection(1, 0, "Home"),//z cesty domu
             new Connection(11, 1, "Return to Road"),//od portalu zpet na cestu
-            new Connection(2, 11,"Return to over World"),
-            new Connection(3, 1, "Return to Road"),//z dolu na cestu
-            new Connection(7, 6, "Return to the Stronghold corridors"),//zpet do cest ve strongholdu
+            new Connection(2, 11,"Over World"),
+            new Connection(3, 1, "Road"),//z dolu na cestu
+            new Connection(7, 6, "Stronghold corridors"),//zpet do cest ve strongholdu
         };
 
         public bool ExistsLocation (int id)
